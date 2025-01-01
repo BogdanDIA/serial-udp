499e903362ccfdb1605ecd9fb2ba7757f2139ff7
BogdanDIA <b_diaconescu@yahoo.com>
2025-01-01 10:25:55 +0200

    Updated run.sh with set-link script

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@
 
 log_info "Entering main loop..."
 
-btattach -B /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2.5:1.0-port0 -P h4 -S 921600 -N &
+#btattach -B /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2.5:1.0-port0 -P h4 -S 921600 -N &
+./app/set-link/set-btattach.sh &
 
 /app/sudp-forwarder -d /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2.6:1.0-port0 -b 921600 -s 192.168.0.142 -N &
 
