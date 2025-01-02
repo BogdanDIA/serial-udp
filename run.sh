6c246861308d506c7ac5b95f50ac7a9e14486b5c
BogdanDIA <b_diaconescu@yahoo.com>
2025-01-02 23:32:41 +0200

    Copy private key and config from external storage upon boot

--- run.sh
+++ run.sh
@@ -10,7 +10,11 @@ log_info "Entering main loop..."
 #/app/sudp-forwarder -d /dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2.6:1.0-port0 -b 921600 -s 192.168.0.142 -N &
 ./app/set-link/set-udp.sh &
 
+cp /share/storage/private-key.pem /root/go/bin/
+cp /share/storage/tesla-ble.conf /root/go/bin/
+cp /share/storage/tesla-ble.conf /root/go/bin/tesla-ble/
+
 while :; do
-  log_info "This is ESP32 HCI Proxy"
+  #log_info "This is ESP32 HCI Proxy"
   sleep 30 
 done
