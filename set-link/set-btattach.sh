2dd10843ec5c065e094dc5323e77d1405742cbce
BogdanDIA <b_diaconescu@yahoo.com>
2025-01-17 15:57:42 +0200

    Updated from the ha_addon branch

--- set-link/set-btattach.sh
+++ set-link/set-btattach.sh
@@ -1,37 +1,41 @@
 #!/bin/ash
 
+CPATH=$(dirname "$0")/serial-udp.conf
+. $CPATH
+. ${SCRIPTS_PATH}/log-def.sh
+
 PARAMS=""
 if [[ -n "$BTATTACH_DEVICE" ]]; then
   PARAMS="$PARAMS -B $BTATTACH_DEVICE"
 else
-  echo no serial device provided, exiting... | tee -a charging-log.txt
-  exit -1
+  log "no serial device provided, exiting..."
+  exit 1
 fi
 
 if [[ -n "$BTATTACH_BAUD" ]]; then
   PARAMS="$PARAMS -S $BTATTACH_BAUD"
 else
-  echo no baud for device provided, exiting... | tee -a charging-log.txt
-  exit -1
+  echo "no baud for device provided, exiting..."
+  exit 1
 fi
 
 if [[ -n "$BTATTACH_PROTOCOL" ]]; then
   PARAMS="$PARAMS -P $BTATTACH_PROTOCOL"
 else
-  echo no protocol provided, exiting... | tee -a charging-log.txt
-  exit -1
+  log "no protocol provided, exiting..."
+  exit 1
 fi
 
 if [[ "$BTATTACH_HWFLOW" -eq 0 ]]; then
   PARAMS="$PARAMS -N"
 fi
 
-echo "`date` set-btattach:btattach" | tee -a charging-log.txt
+log "`date` set-btattach:btattach"
 
 while [[ true ]]; do
   btattach $PARAMS 2>&1
-  echo "set-btattach:btattach crashed with $?.  Respawning.." 2>&1 | tee -a charging-log.txt
+  log "set-btattach:btattach crashed with $?.  Respawning.."
   sleep 2
 done
 
-echo "`date` set-btattach:btattach fail exit" | tee -a charging-log.txt
+log "`date` set-btattach:btattach fail exit"
--- set-link/set-udp.sh
+++ set-link/set-udp.sh
@@ -1,32 +1,37 @@
 #!/bin/ash
 
+CPATH=$(dirname "$0")/serial-udp.conf
+. $CPATH
+. ${SCRIPTS_PATH}/log-def.sh
+
+
 PARAMS=""
 if [[ -n "$SUDPFWD_DEVICE" ]]; then
   PARAMS="$PARAMS -d $SUDPFWD_DEVICE"
 else
-  echo no serial device provided, exiting... | tee -a charging-log.txt
-  exit -1
+  log "no serial device provided, exiting..."
+  exit 1
 fi
 
 if [[ -n "$SUDPFWD_BAUD" ]]; then
   PARAMS="$PARAMS -b $SUDPFWD_BAUD"
 else
-  echo no baud for device provided, exiting... | tee -a charging-log.txt
-  exit -1
+  log "no baud for device provided, exiting..."
+  exit 1
 fi
 
 if [[ -n "$SUDPFWD_SERVER" ]]; then
   PARAMS="$PARAMS -s $SUDPFWD_SERVER"
 else
-  echo no server provided, exiting... | tee -a charging-log.txt
-  exit -1
+  log "no server provided, exiting..."
+  exit 1
 fi
 
 if [[ -n "$SUDPFWD_PORT" ]]; then
   PARAMS="$PARAMS -p $SUDPFWD_PORT"
 else
-  echo no port provided, exiting... | tee -a charging-log.txt
-  exit -1
+  log "no port provided, exiting..."
+  exit 1
 fi
 
 if [[ "$SUDPFWD_HWFLOW" -eq 0 ]]; then
@@ -41,13 +46,13 @@ if [[ "$SUDPFWD_DATADEBUG" -eq 0 ]]; then
   PARAMS="$PARAMS -D"
 fi
 
-echo "`date` set-udp:sudp-forwarder" | tee -a charging-log.txt
+log "`date` set-udp:sudp-forwarder"
 
 while [[ true ]]; do
-  /app/sudp-forwarder $PARAMS 2>&1 | tee -a charging-log.txt
-  echo "set-udp:sudp-forwarder crashed with $?.  Respawning.." 2>&1 | tee -a charging-log.txt
+  ${BIN_PATH}/sudp-forwarder $PARAMS 2>&1 | tee -a ${BIN_PATH}/charging-log.txt
+  log "set-udp:sudp-forwarder crashed with $?.  Respawning.."
   sleep 2
 done
 
-echo "`date` set-udp:sudp-forwarder fail exit" | tee -a charging-log.txt
+log "`date` set-udp:sudp-forwarder fail exit"
 
