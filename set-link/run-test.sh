e99faf3d8b63c1ae58ac8bd28e6781a320652c17
BogdanDIA <b_diaconescu@yahoo.com>
2025-01-17 23:06:40 +0200

    Fixed path of the tests in run-test.sh

--- set-link/run-test.sh
+++ set-link/run-test.sh
@@ -2,5 +2,5 @@ CPATH=$(dirname "$0")/serial-udp.conf
 . $CPATH
 . ${SCRIPTS_PATH}/log-def.sh
 
-${SCRIPTS_PATH}/tests/sudp-test-a -D $BTATTACH_DEVICE -L 256 -M 2000
+${BIN_PATH}/tests/sudp-test-a -D $BTATTACH_DEVICE -L 256 -M 2000
 
