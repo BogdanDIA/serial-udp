c89f75597e3b1eec2e5f3a0027aec2efef4a246d
BogdanDIA <b_diaconescu@yahoo.com>
2026-03-15 19:38:42 +0200

    Add run-observer.sh

---
+++ set-link/run-observer.sh
@@ -0,0 +1,33 @@
+#!/bin/ash
+
+CPATH=$(dirname "$0")/serial-udp.conf
+. $CPATH
+. ${SCRIPTS_PATH}/log-def.sh
+
+DOWN_COUNT=0
+res=""
+
+while :; do
+  HCINUM=$(hciconfig | grep hci | wc -l)
+  HCINUM=$(($HCINUM-1))                 
+  log "HCINUM: $HCINUM"                
+                        
+  #kill btattach after a number or tries                                                                  
+  if [[ $HCINUM -ge 0 ]]; then
+    res=$(hciconfig hci${HCINUM} | grep DOWN)
+    if [[ ! -z $res ]]; then                   
+      log "Trying hciconfig up: hci$HCINUM"      
+      hciconfig hci${HCINUM} up        
+      DOWN_COUNT=$(($DOWN_COUNT+1))    
+      log "DOWN_COUNT: $DOWN_COUNT"
+      if [[ $DOWN_COUNT -ge 6 ]]; then   
+        log "killing btattach"    
+        killall -SIGKILL btattach
+        DOWN_COUNT=0
+      fi
+    else
+      DOWN_COUNT=0
+    fi
+  fi
+  sleep 20
+done
